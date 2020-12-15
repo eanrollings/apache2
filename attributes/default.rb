c34b6e8e7525b930781433e59ea74bdca82223ff
Ean Garcia-Rollings <ean@digitas.com>
2020-12-15 17:30:35 -0500

    Removing alias as a default enabled mod

--- attributes/default.rb
+++ attributes/default.rb
@@ -222,7 +222,7 @@ default['apache']['proxy']['allow_from'] = 'none'
 
 # Default modules to enable via include_recipe
 default['apache']['default_modules'] = %w(
-  status alias auth_basic authn_core authn_file authz_core authz_groupfile
+  status auth_basic authn_core authn_file authz_core authz_groupfile
   authz_host authz_user autoindex deflate dir env mime negotiation setenvif
 )
 
